b8d087568c2c0e6d0a29d7c014bc3d683d52364c
Volodymyr Koukhar <kyxap@ukr.net>
2018-05-28 01:53:17 +0300

    Fix preinstall script

--- preinstall.sh
+++ preinstall.sh
@@ -2,17 +2,32 @@
 
 # Imports
 source platform
+source prettyecho
+source dots
 
 # Pre-installation scripts
-case "$(uname -s)" in
-    Debian*)
-      sudo apt-get update
-      ;;
-    Darwin*)
-      xcode-select --install
-      /usr/bin/ruby -e \
-        "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
-        </dev/null
-      brew doctor
-      ;;
+# General
+touch profile
+setup_dotfile profile
+rm profile
+if [[ -z $(grep '.profile' ~/.bashrc) ]]; then
+  echo 'source ~/.profile' >> ~/.bashrc
+fi
+
+# Platform specific
+case $PLATFORM in
+  Debian*)
+    sudo apt-get update
+    ;;
+  Darwin*)
+    xcode-select --install
+    /usr/bin/ruby -e \
+      "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+      </dev/null
+    brew doctor
+    ;;
+  *)
+    log_error "Platform not supported"
+    exit 1
+    ;;
 esac
