75dd53ff774d4492429498527c269d8c6330c5be
Volodymyr Koukhar <kyxap@ukr.net>
2018-05-28 01:23:02 +0300

    Add .dotfiles config setup function

--- git.sh
+++ git.sh
@@ -2,10 +2,11 @@
 
 source platform
 source prettyecho
+source dots
 
 case $PLATFORM in
   Debian*)
-    sudo apt-get install git -y
+    # sudo apt-get install git -y
     ;;
 
   Darwin*)
@@ -19,16 +20,4 @@ case $PLATFORM in
 
 esac
 
-
-if [ ! -d ~/.dotfiles ]; then
-  mkdir -p ~/.dotfiles
-fi
-if [ -f ~/.gitconfig ]; then
-  echo "Found existing .gitconfig file. Moving to ~/.dotfiles"
-  mv ~/.gitconfig ~/.dotfiles/gitconfig
-  ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-elif [ -f git/gitconfig ]; then
-  echo "Setting up configuration file (~/.gitconfig)"
-  cp git/gitconfig ~/.dotfiles/gitconfig
-  ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-fi
+setup_dotfile git/gitconfig
